7bdb78e3a642e3abaa1a694e4fb7060db75069bb
ubrioniks <ivan_semin_04@mail.ru>
2023-04-08 10:55:51 +0300

    update

---
+++ Testing system/files.h
@@ -0,0 +1,3 @@
+#pragma once
+void code();
+void open_code();
---
+++ Testing system/student.cpp
@@ -0,0 +1,31 @@
+#include "student.h"
+#include <iostream>
+using namespace std;
+void menu(){
+    string login, password;
+    int k;
+    cout << "Введите сначала логин, потом пароль " << endl;
+    do{
+        cin >> login;
+        cin >> password;
+        if (login != "правильный логин")
+            cout << "Доступ запрещен. Введите корректный логин " << endl;
+        if (password != "правильный пароль")
+            cout << "Доступ запрещен. Введите корректный пароль " << endl;
+    }while(login != "правильный логин" || password != "правильный пароль");
+    cout << "Выберите режим 1-тренинг по теме, 2 - тестирование по теме, 3 - итоговый тест" << endl;
+    cin >> k;
+    switch(k){
+        case 1: training_test();
+            break;
+        case 2: theme_test();
+            break;
+        case 3: exam_test();
+            break;
+    }
+}
+
+void tema_trening(){
+    cout << "Выберите тему: 1 - циклы, массивы"<<endl;
+    
+}
