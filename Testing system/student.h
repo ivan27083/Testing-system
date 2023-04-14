00ce44fcd350fdd368285523d7ba7b28e8860f1d
Ann-klek <75799146+Ann-klek@users.noreply.github.com>
2023-04-14 10:56:15 +0300

    Update student.h

--- Testing system/student.h
+++ Testing system/student.h
@@ -3,10 +3,13 @@
 #include <string.h>
 #ifndef student_h
 #define student_h
-void tema_trening();
-void tema_test();
+struct quest;
+struct student;
+void choice(student st);
+void tema_trening(student st);
+void tema_test(student st);
 void finish_test();
 void menu();
-void test(std::string name);
-void train(std::string name);
+void test(std::string name, student st);
+void train(std::string name, student st);
 #endif /* student_h */
